ac5f5bedc7e24e29e94e6ca11ac988e11d513d2f
Jon Jensen <jenseng@gmail.com>
2012-12-03 23:30:30 -0700

    travis-ci

--- spec/schema_dumper_spec.rb
+++ spec/schema_dumper_spec.rb
@@ -6,47 +6,48 @@ require 'active_record/connection_adapters/sqlite3_adapter'
 require 'mysql2'
 require 'rspec'
 require 'hair_trigger'
+require 'yaml'
 
 # for this spec to work, you need to have postgres and mysql installed (in
 # addition to the gems), and you should make sure that you have set up
-# hairtrigger_schema_test dbs in mysql and postgres, along with a hairtrigger
-# user (no password) having appropriate privileges (user needs to be able to
-# drop/recreate this db)
-
-def reset_tmp
-  HairTrigger.model_path = 'tmp/models'
-  HairTrigger.migration_path = 'tmp/migrations'
-  FileUtils.rm_rf('tmp') if File.directory?('tmp')
-  FileUtils.mkdir_p(HairTrigger.model_path)
-  FileUtils.mkdir_p(HairTrigger.migration_path)
-  FileUtils.cp_r('spec/models', 'tmp')
-  FileUtils.cp_r(Dir.glob("spec/migrations#{ActiveRecord::VERSION::STRING < "3.1." ? "-pre-3.1" : ""}/*"), HairTrigger.migration_path)
-end
-
-def initialize_db(adapter)
-  reset_tmp
-  config = {:database => 'hairtrigger_schema_test', :username => 'hairtrigger', :adapter => adapter.to_s, :host => 'localhost'}
-  case adapter
-    when :mysql, :mysql2
-      ret = `echo "drop database if exists hairtrigger_schema_test; create database hairtrigger_schema_test;" | mysql hairtrigger_schema_test -u hairtrigger`
-      raise "error creating database: #{ret}" unless $?.exitstatus == 0
-    when :sqlite3
-      config[:database] = 'tmp/hairtrigger_schema_test.sqlite3'
-    when :postgresql
-      `dropdb -U hairtrigger hairtrigger_schema_test &>/dev/null`
-      ret = `createdb -U hairtrigger hairtrigger_schema_test 2>&1`
-      raise "error creating database: #{ret}" unless $?.exitstatus == 0
-      config[:min_messages] = :error
-  end
-  # Arel has an issue in that it keeps using original connection for quoting,
-  # etc. (which breaks stuff) unless you do this:
-  Arel::Visitors::ENGINE_VISITORS.delete(ActiveRecord::Base) if defined?(Arel)
-  ActiveRecord::Base.establish_connection(config)
-  ActiveRecord::Base.logger = Logger.new('/dev/null')
-  ActiveRecord::Migrator.migrate(HairTrigger.migration_path)
-end
+# appropriate users and permissions. see database.yml for more info
 
 describe "schema" do
+  def reset_tmp
+    HairTrigger.model_path = 'tmp/models'
+    HairTrigger.migration_path = 'tmp/migrations'
+    FileUtils.rm_rf('tmp') if File.directory?('tmp')
+    FileUtils.mkdir_p(HairTrigger.model_path)
+    FileUtils.mkdir_p(HairTrigger.migration_path)
+    FileUtils.cp_r('spec/models', 'tmp')
+    FileUtils.cp_r(Dir.glob("spec/migrations#{ActiveRecord::VERSION::STRING < "3.1." ? "-pre-3.1" : ""}/*"), HairTrigger.migration_path)
+  end
+  
+  def initialize_db(adapter)
+    reset_tmp
+    config = @configs[adapter.to_s].merge({:adapter => adapter.to_s})
+    case adapter
+      when :mysql, :mysql2
+        ret = `echo "drop database if exists #{config['database']}; create database #{config['database']};" | mysql -u #{config['username']}`
+        raise "error creating database: #{ret}" unless $?.exitstatus == 0
+      when :postgresql
+        `dropdb -U #{config['username']} #{config['database']} &>/dev/null`
+        ret = `createdb -U #{config['username']} #{config['database']} 2>&1`
+        raise "error creating database: #{ret}" unless $?.exitstatus == 0
+    end
+    # Arel has an issue in that it keeps using original connection for quoting,
+    # etc. (which breaks stuff) unless you do this:
+    Arel::Visitors::ENGINE_VISITORS.delete(ActiveRecord::Base) if defined?(Arel)
+    ActiveRecord::Base.establish_connection(config)
+    ActiveRecord::Base.logger = Logger.new('/dev/null')
+    ActiveRecord::Migrator.migrate(HairTrigger.migration_path)
+  end
+
+  before :all do
+    @configs = YAML.load_file(File.expand_path(File.dirname(__FILE__) + '/../database.yml'))
+    @configs = @configs[ENV["DB_CONFIG"] || "test"]
+  end
+
   [:mysql, :mysql2, :postgresql, :sqlite3].each do |adapter|
     it "should correctly dump #{adapter}" do
       ActiveRecord::Migration.verbose = false
