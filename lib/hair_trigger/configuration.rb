b93926f1ef603e5f526eb29d59ef2d779a3317de
Jon Jensen <jenseng@gmail.com>
2025-03-31 09:09:08 -0600

    fix typo

--- lib/hair_trigger/configuration.rb
+++ lib/hair_trigger/configuration.rb
@@ -1,6 +1,6 @@
 module HairTrigger
   class Configuration
-    attr_accessor :postgresql_adapters, :mysql_adapters, :sql_adapters
+    attr_accessor :postgresql_adapters, :mysql_adapters, :sqlite_adapters
 
     def initialize(postgresql_adapters: [], mysql_adapters: [], sqlite_adapters: [])
       @postgresql_adapters = postgresql_adapters
